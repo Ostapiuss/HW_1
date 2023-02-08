8b2619c7700b3fdbacd8468214e0baba42b73382
ostap <ostapokolovych@gmail.com>
2023-02-08 18:48:12 +0200

    [FE] HW_1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-import Routes from "./components/Routes";
-import Navigation from "./components/Navigation";
+import Profile from "./components/Profile";
+import Statistics from "./components/Statistics";
+import FriendList from "./components/FriendList";
+import TransitionHistory from "./components/TransitionHistory";
+
+import user from './mocks/user.json';
+import data from './mocks/data.json';
+import friends from './mocks/friends.json';
+import transactions from './mocks/transactions.json'
 
 export const App = () => {
   return (
@@ -11,19 +18,29 @@ export const App = () => {
         style={{
           height: '100vh',
           fontSize: 40,
-          color: '#010101'
+          color: '#010101',
         }}
       >
-        <Navigation />
         <main
           style={{
-            height: '100%',
             display: 'flex',
+            flexDirection: 'column',
             justifyContent: 'center',
             alignItems: 'center',
+            gap: '25px',
+            padding: '50px'
           }}
         >
-          <Routes />
+          <Profile
+            username={user.username}
+            tag={user.tag}
+            location={user.location}
+            avatar={user.avatar}
+            stats={user.stats}
+          />
+          <Statistics title="Upload stats" stats={data} />
+          <FriendList friends={friends} />
+          <TransitionHistory items={transactions} />
         </main>
       </div>
     </>
